761e1355c529d1801a2f70a7396f4b07b0c5eea3
lesitoan <lesitoan.media2gmail.com>
2023-03-25 20:48:15 +0700

    router_MVC

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,5 @@
 import express from "express";
-import getHomePage from "../controller/homeController";
+import homeController from "../controller/homeController";
 
 let router = express.Router();
 
