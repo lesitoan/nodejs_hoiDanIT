d7acc6fd95ea87742f80ab0c66d4efdffa73bac5
lesitoan <lesitoan.media@gmail.com>
2023-03-30 00:54:03 +0700

    video13_CRUD

--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,11 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage);
     router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+
+    router.post('/update-user', homeController.postUpdateUser);
+
 
     return app.use('/', router);
 }
